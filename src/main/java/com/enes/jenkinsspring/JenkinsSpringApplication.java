e9579dc56bcbdfcf1ffa77ecb896bc39773a9cf0
enes <enesuluc04@gmail.com>
2026-01-30 20:55:34 +0300

    test

--- src/main/java/com/enes/jenkinsspring/JenkinsSpringApplication.java
+++ src/main/java/com/enes/jenkinsspring/JenkinsSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsSpringApplication {
-
+asd
     public static void main(String[] args) {
         SpringApplication.run(JenkinsSpringApplication.class, args);
     }
