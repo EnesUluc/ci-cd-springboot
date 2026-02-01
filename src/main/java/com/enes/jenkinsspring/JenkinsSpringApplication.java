2b7cc78775a39ad710cbd457fa640a124f72a84f
enes <enesuluc04@gmail.com>
2026-02-01 19:14:08 +0300

    adas

--- src/main/java/com/enes/jenkinsspring/JenkinsSpringApplication.java
+++ src/main/java/com/enes/jenkinsspring/JenkinsSpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsSpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(Stri ng[] args) {
         SpringApplication.run(JenkinsSpringApplication.class, args);
     }
 
